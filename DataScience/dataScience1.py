632434d2e2dab057816297b024c0fe001e81baf9
André Marques de Oliveira <AndreMarxs@users.noreply.github.com>
2019-12-29 16:16:21 -0300

    Subindo novo ambiente python para programar

---
+++ DataScience/dataScience1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+import numpy as nm
+uri = "https://raw.githubusercontent.com/alura-cursos/introducao-a-data-science/master/aula4.1/movies.csv"
+print("Acabou")
+pd.read_csv(uri)
+    
